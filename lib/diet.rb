cc38fb96f17f102d4f470f9d9e20be024fdf0f49
Derek Kastner <dkastner@gmail.com>
2010-09-27 11:45:07 -0400

    Don't need diet_model

--- lib/diet.rb
+++ lib/diet.rb
@@ -3,15 +3,5 @@ require 'emitter'
 module BrighterPlanet
   module Diet
     extend BrighterPlanet::Emitter
-
-    def self.diet_model
-      if Object.const_defined? 'Diet'
-        ::Diet
-      elsif Object.const_defined? 'DietRecord'
-        DietRecord
-      else
-        raise 'There is no diet model'
-      end
-    end
   end
 end
