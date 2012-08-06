0b71083348622f81f7ce65314fc37eeb17207c18
Derek Kastner <dkastner@gmail.com>
2012-08-06 13:53:35 -0500

    Enumerate emitter requires

--- lib/diet.rb
+++ lib/diet.rb
@@ -1,5 +1,11 @@
 require 'emitter'
 
+require 'diet/impact_model'
+require 'diet/characterization'
+require 'diet/data'
+require 'diet/relationships'
+require 'diet/summarization'
+
 module BrighterPlanet
   module Diet
     extend BrighterPlanet::Emitter
